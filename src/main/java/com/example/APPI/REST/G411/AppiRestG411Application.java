02fe580936ff4321cacbcb7c4b34f0c290e2000d
Luis-Ospino <luisospinodev@gmail.com>
2024-11-20 11:56:47 -0500

    ACTIVANDO EL CORS PARA PROTEGER LA API Y CONECTAR CON EL CLIENTE

--- src/main/java/com/example/APPI/REST/G411/AppiRestG411Application.java
+++ src/main/java/com/example/APPI/REST/G411/AppiRestG411Application.java
@@ -2,6 +2,9 @@ package com.example.APPI.REST.G411;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AppiRestG411Application {
@@ -9,4 +12,20 @@ public class AppiRestG411Application {
 	public static void main(String[] args) {
 		SpringApplication.run(AppiRestG411Application.class, args);
 	}
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")  // Permite todas las rutas
+						.allowedOrigins("*")  // Permite cualquier origen
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Métodos permitidos
+						.allowedHeaders("*");  // Permite todos los encabezados
+			}
+		};
+	}
+
+
 }
